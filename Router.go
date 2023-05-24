4f0b102a38d3e2ed834766721fad61b341e4e3b0
CoolTrol <bohdan1234yunior@gmail.com>
2023-05-24 20:17:34 +0300

    Custom router implementation

---
+++ Router.go
@@ -0,0 +1,30 @@
+package main
+
+import "net/http"
+
+type Route struct {
+	Method  string
+	Path    string
+	Handler http.HandlerFunc
+}
+type Router struct {
+	Routes []Route
+}
+
+func (r *Router) addNewRoute(requestType, path string, handler http.HandlerFunc) {
+	route := Route{
+		requestType,
+		path,
+		handler,
+	}
+	r.Routes = append(r.Routes, route)
+}
+func (r *Router) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	for _, route := range r.Routes {
+		if route.Method == request.Method && route.Path == request.URL.Path {
+			route.Handler(writer, request)
+			return
+		}
+	}
+	http.NotFound(writer, request)
+}
